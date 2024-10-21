b8e74fd5dcc25b86f00b942653f54379e1424034
maxpawgdbs <maxim.taranenko17112008@gmail.com>
2024-10-21 21:22:59 +0300

    test

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
-import django.urls
 from django.shortcuts import reverse
 from django.test import Client, TestCase
+import django.urls
 from parametrize import parametrize
 
 
