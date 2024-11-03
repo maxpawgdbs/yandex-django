8db5440e05b2ebf36a9f31094127e62172e5a92f
maxpawgdbs <maxim.taranenko17112008@gmail.com>
2024-11-03 22:06:44 +0300

    6th week 1st task 6th try

--- lyceum/feedback/tests.py
+++ lyceum/feedback/tests.py
@@ -11,14 +11,12 @@ class FormTest(django.test.TestCase):
     def test_labels(self):
         url = django.shortcuts.reverse("feedback:feedback")
         response = django.test.Client().get(url)
-        self.assertEqual(response.context["form"]["name"].label, "Имя")
         self.assertEqual(response.context["form"]["text"].label, "Фидбек")
         self.assertEqual(response.context["form"]["mail"].label, "Email")
 
     def test_help_texts(self):
         url = django.shortcuts.reverse("feedback:feedback")
         response = django.test.Client().get(url)
-        self.assertEqual(response.context["form"]["name"].help_text, "Имя")
         self.assertEqual(response.context["form"]["text"].help_text, "Фидбек")
         self.assertEqual(response.context["form"]["mail"].help_text, "Email")
 
@@ -26,7 +24,7 @@ class FormTest(django.test.TestCase):
         url = django.shortcuts.reverse("feedback:feedback")
         response = django.test.Client().post(
             url,
-            data={"name": "123", "text": "123", "mail": "123@123.123"},
+            data={"text": "123", "mail": "123@123.123"},
             follow=True,
         )
         self.assertRedirects(response, url)
@@ -35,7 +33,7 @@ class FormTest(django.test.TestCase):
         url = django.shortcuts.reverse("feedback:feedback")
         response = django.test.Client().post(
             url,
-            data={"name": "123", "text": "123", "mail": "123123.123"},
+            data={"text": "123", "mail": "123123.123"},
             follow=True,
         )
         self.assertTrue(response.context["form"].has_error("mail"))
